3e21f0c82e1f7bf1d4e45d72009b35c89dc25957
rebelo-dev <joao.rebelo.dev@gmail.com>
2026-04-27 21:07:55 +0100

    unchanged id's to uuid
    just added some comments so i know i left those purposely at this time

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // might change to uuid in future, right now it doesn't hinder the app 
             $table->string('uuid')->unique();
             $table->text('connection');
             $table->text('queue');
--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // might change to uuid in future, right now it doesn't hinder the app 
             $table->uuidMorphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
