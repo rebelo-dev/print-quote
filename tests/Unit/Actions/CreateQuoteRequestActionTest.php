be37a60cb4996430b30754aed093ed2072a86d12
rebelo-dev <joao.rebelo.dev@gmail.com>
2026-05-05 20:16:33 +0100

    feat: Policy and Action tests

---
+++ tests/Unit/Actions/CreateQuoteRequestActionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit\Actions;
+
+use PHPUnit\Framework\TestCase;
+
+class CreateQuoteRequestActionTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_create_quote_request_action(): void
+    {
+        $this->assertTrue(true);
+    }
+}
